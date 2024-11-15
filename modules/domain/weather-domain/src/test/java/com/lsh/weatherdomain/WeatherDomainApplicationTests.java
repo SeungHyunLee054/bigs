19f6daa582e649cf55337e0c228dbd7acf75cf9d
SeungHyunLee054 <yellow13lsh@naver.com>
2024-11-16 02:11:19 +0900

    weather api - 구조변경

---
+++ modules/domain/weather-domain/src/test/java/com/lsh/weatherdomain/WeatherDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsh.weatherdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
